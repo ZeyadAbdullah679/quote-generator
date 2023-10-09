bdd132543acdd13f112b64e7b5b749b05a3387bc
Zeyad Abdullah <zeyadgalal93@gmail.com>
2023-10-09 21:42:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/quotegenerator/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.example.quotegenerator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val primary = Color(0xFF8249B5)
+val primaryContainer = Color(0xFFFBFBFB)
+val secondaryContainer = Color(0xB2FBFBFB)
+val tertiaryContainer = Color(0xFF323232)
+
+
+val primaryFont = Color(0xFF323232)
+val secondaryFont = Color(0xFF8249B5)
+val subFont = Color(0xB2323232)
